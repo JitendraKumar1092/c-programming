44b75b5335300cf8735e73c750768431ed580ba4
JitendraKumar1092 <76446860+JitendraKumar1092@users.noreply.github.com>
2021-03-16 00:27:43 +0530

    digit_count.c
    Sir is it okay?

--- digits_count.c
+++ digits_count.c
@@ -1,32 +1,10 @@
-// program to count the digits of given integer value
 
 #include<stdio.h>
-
-void main()
-{
-    int num, count=0;
-    printf("enter the number ");
-    scanf("%d", &num);
-    if(num==0)
-        count++;
-    while(num != 0)
-    {
-        num = num/10;
-        ++count;
-    }
-    printf("%d", count);
+void main(){
+int num;
+scanf("%d",num);
+Digit = printf("%d",num);
+printf("\n there are %d digits in number",Digit);
 }
-
-
-step1: start
-step2: declare the number n
-step3: read the number n
-step4: init count=0
-step5: while n > 0
-            n /= 10
-            count++
-step6: display count 
-step7: stop           
-
 
 
